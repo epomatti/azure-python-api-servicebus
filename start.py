26ac0928577c4a41505abc8334f465d98a72428b
Evandro Pomatti <evandro.pomatti@hbsis.com.br>
2019-09-23 11:43:13 -0300

    json post

--- start.py
+++ start.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/detect", methods=['POST'])
+def detect():
+    print(request.json)
 
 if __name__ == "__main__":
     app.run()
